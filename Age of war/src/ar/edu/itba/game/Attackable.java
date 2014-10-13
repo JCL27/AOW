cb37983136e5c894f05ace143cf08ec1dab08ecb
mati <matimercado06@hotmail.com>
2014-10-13 11:23:06 -0300

    Lepo forro

--- Age of war/src/ar/edu/itba/game/Attackable.java
+++ Age of war/src/ar/edu/itba/game/Attackable.java
@@ -1,7 +1,7 @@
 package ar.edu.itba.game;
 
 import exceptions.DeadUnitException;
-
+//ALTO COMMENT
 public interface Attackable {
 	public void receiveDamage(int damage) throws DeadUnitException;
 	public Element getElement();
